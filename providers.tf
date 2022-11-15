1162a6f6951191006215296f3265fddd62b0de0a
murtaza <murtaza@murtazas-MacBook-Pro.local>
2022-11-15 09:52:12 -0600

    final commit

--- providers.tf
+++ providers.tf
@@ -1,5 +1,3 @@
 provider "aws" {
-  region = "us-east-1"
-  shared_config_files      = ["/Users/murtaza/.aws/config"]
-  shared_credentials_files = ["/Users/murtaza/.aws/credentials"]
+  region     = "us-east-1"
 }
