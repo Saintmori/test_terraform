874b4646860bbf275e658a54d570d988c0bcb12d
murtaza <murtaza@murtazas-MacBook-Pro.local>
2022-11-15 09:15:35 -0600

    modified tf,file

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = var.region
   profile = var.aws_profile
-  shared_credentials_file = var.shared_credentials_file
+  shared_credentials_files = var.shared_credentials_files
 }
--- variables.tf
+++ variables.tf
@@ -8,7 +8,7 @@ variable "aws_profile" {
     type = string
     default = "murtaza_profile"
 }
-variable "shared_credentials_file" {
+variable "shared_credentials_files" {
     type = string
     description = " this is the credential file"
     default = "/Users/murtaza/.aws/credential"
