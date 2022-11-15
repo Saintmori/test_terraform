8a286b3ee60c8617cdc4a62194b64b221cc74410
murtaza <murtaza@murtazas-MacBook-Pro.local>
2022-11-15 09:28:14 -0600

    fourth commit

--- providers.tf
+++ providers.tf
@@ -1,5 +1,4 @@
 provider "aws" {
   shared_config_files      = ["/Users/murtaza/.aws/config"]
   shared_credentials_files = ["/Users/murtaza/.aws/credentials"]
-  profile                  = "murtaza_profile"
 }
